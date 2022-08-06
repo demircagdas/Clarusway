9c9856a62bbd7becb55fd9209b6b59f549921e77
demircagdas <24cagdasdemir@gmail.com>
2022-08-06 13:57:56 +0300

    js3

--- js/session1/app.js
+++ js/session1/app.js
@@ -1,1 +1,14 @@
-alert('booo')
+//alert('booo')
+
+
+var MyNumber1 = 11
+console.log(MyNumber1)
+
+var MyNumber1 = 22
+console.log(MyNumber1)
+
+const student ={
+    name: 'Cagdas',
+    age:30
+}
+console.log(student)
