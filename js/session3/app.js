f5eed4fa129be5ac1f247ccf2031126fbff3f548
demircagdas <24cagdasdemir@gmail.com>
2022-08-11 21:56:10 +0300

    js1

---
+++ js/session3/app.js
@@ -0,0 +1,22 @@
+console.log(3 == '3')
+console.log(3 === '3')
+console.log(3 != '3')
+console.log(3 !== '3')
+console.log(3 && '3')
+console.log(3 || '3')
+
+const nullAndString = null ?? "Hello World";
+console.log(`nullAndString -> ${nullAndString}`);
+
+const result = 0 ?? 42;
+console.log(`result -> ${result}`);
+
+// const nullValue = null;
+// const emptyText = "";
+// const someNumber = 42;
+
+// const resultA = nullValue ?? "Hello World";
+// const resultB = nullValue ?? "Hello World";
+// const resultC = nullValue ?? "Hello World";
+
+
