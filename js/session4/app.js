e93cf6139b7fd0600b4ae75063709b71ea76e760
demircagdas <24cagdasdemir@gmail.com>
2022-08-15 21:59:41 +0300

    js2

---
+++ js/session4/app.js
@@ -0,0 +1,39 @@
+// * ===============================================
+// *                  KARAR YAPILARI
+// * ===============================================
+
+//**************** IF-ELSE *******************
+
+console.log("****** CONDITIONS *******");
+
+
+// bir sayının pozitif ya da negatif olduğunu görelim
+
+// const sayi = prompt("Bir sayı giriniz:")
+// if (sayi > 0) {
+//     console.log(`${sayi} pozitif bir sayıdır.`)
+// }else if(sayi == 0){
+//     console.log(`${sayi} sayı 0'a eşittir.`)
+// }else {
+//     console.log(`${sayi} negatif bir sayıdır.`)
+// }
+
+// console.log(sayi, typeof sayi)
+
+
+
+
+
+// 3 sayı iste ve en büyüğünü yazdır.
+
+// const n1 = prompt("Sayi1")
+// const n2 = prompt("Sayi2")
+// const n3 = prompt("Sayi3")
+
+// if (n1 >= n2 && n1 >= n3) {
+//     console.log(`${n1} en büyük sayıdır.`);
+// } else if (n2 >= n1 && n2 >= n3){
+//     console.log(`${n2} en büyük sayıdır.`);
+// } else if (n3 >= n2 && n3 >= n1){
+//     console.log(`${n3} en büyük sayıdır.`);
+// }
