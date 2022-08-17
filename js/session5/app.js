180e5f39c0f911b2f1aa4513655cdce361641b3b
demircagdas <24cagdasdemir@gmail.com>
2022-08-17 22:00:27 +0300

    js3

--- js/session5/app.js
+++ js/session5/app.js
@@ -26,3 +26,56 @@ for(let i = 1 ; i <= 10; i++){
 //     const rastgele = Math.floor(Math.random() *100)
 //     console.log(rastgele);
 // }
+
+
+
+
+
+// Girilen sayının asal olup olmadığını for döngüsü ile yazınız
+
+
+// const sayi = +prompt("Pozitif bir sayı giriniz:")
+// let asal = true;
+
+// if(sayi <= 0){
+//     alert
+//     ("Sayı 0'dan büyük olmalıdır.")
+// }else{
+//     for(let i=2 ; i<sayi ; i++){
+//         if(sayi % i === 0){
+//             asal = false;
+//             break;
+//         }
+//     }
+//     const sonuc = asal === true ? "Asaldır" :"Asal Değildir"
+//     console.log(`${sayi} ${sonuc}`);
+// }
+
+
+
+
+
+// * ============================================
+// *            DONGULER(LOOPS)- WHILE
+// * ============================================
+
+console.log("****** WHILE *******");
+
+//? ORNEK: Kullanicidan 0-100 arasinda bir not isteyen ve girilen not
+//? 0-100'den farkli ise Kullaciniyi uyararak yeniden 0-100 arasinda
+//? not girmeye zorlayan kodu while dongusu ile yaziniz.
+
+
+// let not = +prompt("Lütfen 0-100 arasında bir not giriniz.")
+
+// while (not < 0 || not > 100){
+//     console.log("Not *-100 arasında olmalıdır.");
+//     not = +prompt("Lütfen 0-100 arasında bir not giriniz.")
+// }
+// console.log("Girdiğiniz not:", not);
+
+
+
+
+
+console.log("****** DO WHILE *******");
