4793215fa544510f697de4b475e289f445d2fed8
demircagdas <24cagdasdemir@gmail.com>
2022-08-17 20:54:10 +0300

    js1

--- js/session5/app.js
+++ js/session5/app.js
@@ -1,1 +1,28 @@
 console.log('bom');
+
+for(let i = 1 ; i <= 10; i++){
+    console.log("Merhaba");
+}
+
+
+
+
+//  1'den başlayarak n'e kadar hepsini topla
+
+// let toplam = 0
+// const n = +prompt("Bir sayı giriniz:")
+// for(i=1 ; i <= n ; i++){
+// //    toplam = toplam + i;  alttakiyle aynı syntax
+//    toplam += i
+//     console.log('sonuç:', toplam);
+// }
+
+
+
+
+// 0-100 arasında 10 tane random sayı üreten bir kod yaz
+
+// for (let i = 1; i <= 10; i++) {
+//     const rastgele = Math.floor(Math.random() *100)
+//     console.log(rastgele);
+// }
