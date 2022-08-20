5dc9a9b648e1af102b46490233cea60254945eff
demircagdas <24cagdasdemir@gmail.com>
2022-08-20 13:21:07 +0300

    js2

--- js/session6/app.js
+++ js/session6/app.js
@@ -4,3 +4,36 @@ function yazdir () {
 
 yazdir();
 yazdir();
+
+
+
+
+
+function selamla(ad, soyad) {
+    console.log(`Merhaba ${ad} ${soyad}`);
+}
+
+
+selamla("Çağdaş", "Demir")
+
+
+
+
+function yashesapla (isim, dogumtarihi) {
+    const sonuc = `${isim}, ${2022 - dogumtarihi} yaşındadır`
+    return sonuc
+}
+
+const cagdasyas = yashesapla("Çağdaş", "1992")
+console.log(cagdasyas);
+
+
+
+
+
+function tekcift(sayi) {
+    return sayi % 2 ? "Tektir" : "Çifttir"
+}
+
+tekcift(5);
+tekcift(2);
