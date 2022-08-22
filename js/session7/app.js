f42cf543c3a9af4f85ec8bd740c4333ae944d1c9
demircagdas <24cagdasdemir@gmail.com>
2022-08-22 21:09:40 +0300

    js1

---
+++ js/session7/app.js
@@ -0,0 +1,17 @@
+
+
+// Silindir Hacmi
+
+
+// const r = Number(prompt("yaricap giriniz"))
+// const h = Number(prompt("yukseklik giriniz"))
+
+// const hacimhesapla = (r, h) => Math.PI * r * r * h;
+
+// console.log(`${r} , ${h} => Hacmi: ${hacimhesapla(r, h)}`);
+
+
+
+
+
+
