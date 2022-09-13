5f81a1edcb2fb505004fad7e74a0a4b75d6c139c
demircagdas <24cagdasdemir@gmail.com>
2022-09-13 20:35:46 +0300

    ..

--- js/session15/app.js
+++ js/session15/app.js
@@ -6,12 +6,24 @@
 
 //? 1-100 arasında rastgele bir sayı tut
 
+const randomNumber = math.floor( math.random() * 100) +1
 
+console.log(randomNumber);
 
 
 
 
 //? Check butonuna basıldığında kontrolleri yap
+
+document.querySelector(".check-btn").addEventListener("click", () => {
+    const guessInput =document.querySelector(".guess-input")
+
+
+})
+
+
+
+
 //! İnput girilmezse kullanıcıya uyarı ver.
 
 //! eğer rastgele == input.value 
@@ -24,14 +36,16 @@
 
 
 //! değilse
+//! eger score > 0
 //! score = score -1
 //? Eğer rastgele sayı < input.value
 //! Azalt
 //?Değilse
 //! Arttır
+//! değilse
+//? üzgünüz, kaybettiniz.
 
 
 
 
-
-//? 
+//? Again'e basıldığında 
