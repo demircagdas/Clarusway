4a3e057168d942cc43fcc3e3b539dc55b2405cd9
demircagdas <24cagdasdemir@gmail.com>
2022-09-13 20:24:52 +0300

    guess

---
+++ js/session15/game.js
@@ -0,0 +1,37 @@
+//* =====================================
+//*  GUESS MY NUMBER
+//*  Game Logic
+//*======================================
+
+
+//? 1-100 arasında rastgele bir sayı tut
+
+
+
+
+
+
+//? Check butonuna basıldığında kontrolleri yap
+//! İnput girilmezse kullanıcıya uyarı ver.
+
+//! eğer rastgele == input.value 
+//? Tebrikler, bildiniz.
+//! Background = Green
+//? eğer score > topScore
+//?      topScore = score
+//?  secret_number = görünür
+
+
+
+//! değilse
+//! score = score -1
+//? Eğer rastgele sayı < input.value
+//! Azalt
+//?Değilse
+//! Arttır
+
+
+
+
+
+//? 
